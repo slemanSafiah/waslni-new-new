0d47af7c7f320e3188af6596d3c6c3c71211a036
slemanSafiah <sleman-safiah@hotmail.com>
2020-09-13 12:15:42 +0300

    first commit

--- routes/trip.js
+++ routes/trip.js
@@ -5,8 +5,9 @@ const Driver = require('../models/Driver');
 const User = require('../models/User');
 const request = require('request');
 const http = require('https');
+const axios = require('axios');
 var ACCESS_TOKEN = "pk.eyJ1IjoidGFtYXJhamFtbW91bCIsImEiOiJja2NxMG1kNm8xMGtzMnNsbWExbGtpbm8zIn0.bgQ23ChS-u88zfS7dm6Fbw";
-var str;
+
 
 var trps = [];
 var tr = {};
@@ -16,7 +17,6 @@ router.post('/get_trips_by_driver', async (req, res) => {
             driver_number: req.body.number
         });
         Promise.all(trips.map(item => {
-
             return anAsyncFunction(item);
         })).then(() => res.json({
             sucess: 1,
@@ -37,10 +37,12 @@ router.post('/get_trips_by_user', async (req, res) => {
         const trips = await Trip.find({
             user_number: req.body.number
         });
-        res.json({
+        Promise.all(trips.map(item => {
+            return anAsyncFunction(item);
+        })).then(() => res.json({
             sucess: 1,
-            data: trips
-        });
+            data: trps
+        }));
     } catch (err) {
         res.json({
             sucess: 0,
@@ -52,41 +54,14 @@ router.post('/get_trips_by_user', async (req, res) => {
 
 const anAsyncFunction = async item => {
 
-    await http.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' + item.source_long + ', ' + item.source_lat + '.json?access_token=' + ACCESS_TOKEN,
-        async resp => {
-            var jsonData = '';
-            await resp.on("data", (chunk) => {
-                jsonData += chunk;
-            }).on("end", () => {
-                var obj = JSON.parse(jsonData);
-                tr.from = obj.features[0].place_name;
-                console.log("w1");
-            });
-        }).on("error", (err) => {
-        console.log(err);
-    })
-    await http.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' + item.dest_long + ', ' + item.dest_lat + '.json?access_token=' + ACCESS_TOKEN,
-        async resp => {
-            var obj;
-            var jsonData = '';
-            await resp.on("data", (chunk) => {
-                jsonData += chunk;
-            }).on("end", () => {
-                obj = JSON.parse(jsonData);
-                tr.to = obj.features[0].place_name;
-                console.log("w2");
-                return Promise.resolve('ok');
-
-            });
-            console.log("w3");
-        }).on("error", (err) => {
-        console.log(err);
-    });
+    var b = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' + item.source_long + ', ' + item.source_lat + '.json?access_token=' + ACCESS_TOKEN);
+    var a = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' + item.dest_long + ', ' + item.dest_lat + '.json?access_token=' + ACCESS_TOKEN);
+    if (b.data.features.length != 0)
+        tr.from = b.data.features[0].place_name;
+    if (a.data.features.length != 0)
+        tr.to = a.data.features[0].place_name;
     tr.driver_number = item.driver_number;
     tr.user_number = item.user_number;
-    //  tr.from = "";
-    //  tr.to = "";
-    console.log("chelsea");
     trps.push(tr);
     return Promise.resolve('ok');
 }
